796cdf70c1e3138c294777f7f791b7f9d822a916
Paul <peru77@gmx.at>
2019-05-14 00:16:03 +0200

    started ausgleich

--- main.py
+++ main.py
@@ -6,19 +6,18 @@ from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 
 
-def create_tls_data(m_x, m_y, m_z, radius, verbose=False):
+def create_tls_data(m_x, m_y, m_z, radius, verbose=False, stativ=False):
     # zylinder as stativ aproximation
     radius_zylinder = radius / 3
     height = radius * 3
 
-    x_coords_zyl = []
-    y_coords_zyl = []
-    z_coords_zyl = []
+
 
     x_coords = []
     y_coords = []
     z_coords = []
 
+    #sphere
     for phi in range(1, 360, 12):
         for theta in range(1, 360, 12):
             x = m_x + radius * numpy.sin(theta * (numpy.pi / 180)) * numpy.cos(phi * (numpy.pi / 180))
@@ -35,24 +34,30 @@ def create_tls_data(m_x, m_y, m_z, radius, verbose=False):
             y_coords.append(y)
             z_coords.append(z)
 
-    for phi in range(1, 360, 12):
-        for h in range(m_z - height, m_z - radius, 1):
-            x = m_x + radius_zylinder * numpy.cos(phi * (numpy.pi / 180))
-            y = m_y + radius_zylinder * numpy.sin(phi * (numpy.pi / 180))
-            z = h
+    x_arr = numpy.asarray(x_coords) + numpy.random.rand(len(x_coords))*1.5
+    y_arr = numpy.asarray(y_coords) + numpy.random.rand(len(x_coords))*1.5
+    z_arr = numpy.asarray(z_coords) + numpy.random.rand(len(x_coords))*1.5
 
-            x_coords_zyl.append(x)
-            y_coords_zyl.append(y)
-            z_coords_zyl.append(z)
+    #stativ
+    if stativ:
+        x_coords_zyl = []
+        y_coords_zyl = []
+        z_coords_zyl = []
+        for phi in range(1, 360, 12):
+            for h in range(m_z - height, m_z - radius, 1):
+                x = m_x + radius_zylinder * numpy.cos(phi * (numpy.pi / 180))
+                y = m_y + radius_zylinder * numpy.sin(phi * (numpy.pi / 180))
+                z = h
+
+                x_coords_zyl.append(x)
+                y_coords_zyl.append(y)
+                z_coords_zyl.append(z)
+
+                x_arr_zyl = numpy.asarray(x_coords_zyl) + numpy.random.rand(len(x_coords_zyl)) * 1.5
+                y_arr_zyl = numpy.asarray(y_coords_zyl) + numpy.random.rand(len(x_coords_zyl)) * 1.5
+                z_arr_zyl = numpy.asarray(z_coords_zyl) + numpy.random.rand(len(x_coords_zyl)) * 1.5
 
 
-    x_arr = numpy.asarray(x_coords) + numpy.random.rand(len(x_coords))
-    y_arr = numpy.asarray(y_coords) + numpy.random.rand(len(x_coords))
-    z_arr = numpy.asarray(z_coords) + numpy.random.rand(len(x_coords))
-
-    x_arr_zyl = numpy.asarray(x_coords_zyl) + numpy.random.rand(len(x_coords_zyl))
-    y_arr_zyl = numpy.asarray(y_coords_zyl) + numpy.random.rand(len(x_coords_zyl))
-    z_arr_zyl = numpy.asarray(z_coords_zyl) + numpy.random.rand(len(x_coords_zyl))
 
     if verbose:
         print(" len of x ", len(x_coords))
@@ -60,7 +65,8 @@ def create_tls_data(m_x, m_y, m_z, radius, verbose=False):
         fig = plt.figure()
         ax = fig.gca(projection='3d')
         ax.scatter3D(x_arr, y_arr, z_arr, c=z_arr, cmap='jet', linewidths=0.5, label="Test Sphere Points")
-        ax.scatter3D(x_arr_zyl, y_arr_zyl, z_arr_zyl, c=z_arr_zyl, cmap='viridis', linewidths=0.5,
+        if stativ:
+            ax.scatter3D(x_arr_zyl, y_arr_zyl, z_arr_zyl, c=z_arr_zyl, cmap='viridis', linewidths=0.5,
                      label="Test Zylinder Points")
         ax.set_title("3D Scatter Plot of the Test Sphere")
         ax.set_xlabel("X Coorinates")
@@ -69,11 +75,17 @@ def create_tls_data(m_x, m_y, m_z, radius, verbose=False):
         ax.legend()
 
         plt.show()
-    x_data = numpy.hstack((x_arr, x_arr_zyl))
-    y_data = numpy.hstack((y_arr, y_arr_zyl))
-    z_data = numpy.hstack((z_arr, z_arr_zyl))
 
-    return x_data, y_data, z_data,
+    if stativ:
+        x_data = numpy.hstack((x_arr, x_arr_zyl))
+        y_data = numpy.hstack((y_arr, y_arr_zyl))
+        z_data = numpy.hstack((z_arr, z_arr_zyl))
+    else:
+        x_data = x_arr
+        y_data = y_arr
+        z_data = z_arr
+
+    return x_data, y_data, z_data
 
 if __name__ == "__main__":
 
@@ -83,7 +95,46 @@ if __name__ == "__main__":
     sphere_y = 16
     sphere_z = 99
 
-    tls_x, tls_y, tls_z = create_tls_data(sphere_x, sphere_y, sphere_z, sphere_radius, True)
+    # create TLS Data
+    tls_x, tls_y, tls_z = create_tls_data(sphere_x, sphere_y, sphere_z, sphere_radius)
+
+    r_init = numpy.ones(len(tls_y))
+    v_init = numpy.zeros(len(tls_y))
+
+    # näherungswerte
+    x_d_init = numpy.array([18,20,150,8])
+
+    r_init = numpy.sqrt((tls_x + v_init - x_d_init[0])**2 +
+                        (tls_y + v_init - x_d_init[1])**2 +
+                        (tls_y + v_init - x_d_init[2])**2 )
+
+
+    # Kovarianzmatrix der Beobachtungen
+    SIGMA_l = numpy.eye(len(tls_x), len(tls_x))
+
+    # Designmatrix A
+
+    A = numpy.array([ -(tls_x + v_init - x_d_init[0])/r_init, -(tls_y + v_init - x_d_init[1])/r_init, -(tls_z + v_init - x_d_init[2])/r_init, r_init]).T
+
+    B = numpy.eye(len(tls_x)) * A
+    print(B)
+
+    w = (tls_x + v_init - x_d_init[0]) * (tls_x - x_d_init[0]) +\
+        (tls_y + v_init - x_d_init[1]) * (tls_y - x_d_init[1]) +\
+        (tls_z + v_init - x_d_init[2]) * (tls_z - x_d_init[2])
+
+    print("A",type(A), A.shape)
+    print("w", type(w), w.shape)
+    print("SIG", type(SIGMA_l), SIGMA_l.shape)
+    # Normal gleichungsmatrix
+    N = -numpy.linalg.inv(numpy.dot(numpy.dot(A.T, SIGMA_l), A))
+    widerspruchteil = numpy.dot(numpy.dot(A.T,SIGMA_l),w)
+    x_d =  numpy.dot(N,widerspruchteil)
+
+    print(x_d)
+    print(SIGMA_l.shape)
+    print(A.shape)
+
 
 
 
